b654e3c598108d9110fd700809587302aa0e2c05
Eliwelton Moreira <eliwelton.moreira@outlook.com>
2022-10-25 07:42:32 -0300

    function findZipCodeCep

--- find-zipcode-cep-http/main.go
+++ find-zipcode-cep-http/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", FindZipCodeCEPHandler)
@@ -21,4 +38,25 @@ func FindZipCodeCEPHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello world"))
+}
+
+func FindZipCodeCep(zipcode string) (*ViaCEP, error) {
+	resp, err := http.Get("http://viacep.com.br/ws/" + zipcode + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var c ViaCEP
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
 }
