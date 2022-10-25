1e365aeb40bedbf6a2efeda0349a374d9cdb5e62
Eliwelton Moreira <eliwelton.moreira@outlook.com>
2022-10-25 07:35:42 -0300

    header manipulation

--- find-zipcode-cep-http/main.go
+++ find-zipcode-cep-http/main.go
@@ -3,10 +3,22 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", FindZipCodeCEP)
+	http.HandleFunc("/", FindZipCodeCEPHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func FindZipCodeCEP(w http.ResponseWriter, r *http.Request) {
+func FindZipCodeCEPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello world"))
 }
