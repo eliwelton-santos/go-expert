f33e2b8198c4bb357ee884366887031fd63e2706
Eliwelton Moreira <eliwelton.moreira@outlook.com>
2022-11-18 19:57:31 -0300

    add chi

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Eliwelton-The-Espada/go-expert/apis/internal/entity"
 	"github.com/Eliwelton-The-Espada/go-expert/apis/internal/infra/database"
 	"github.com/Eliwelton-The-Espada/go-expert/apis/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -25,6 +27,9 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
--- apis/go.mod
+++ apis/go.mod
@@ -24,6 +24,7 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.3.0
 	github.com/hashicorp/hcl v1.0.0 // indirect
